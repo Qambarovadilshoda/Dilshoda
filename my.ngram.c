298131c3013c268f302c45a5e85f87c9451dc4bb
Dilshoda <158685322+Qambarovadilshoda@users.noreply.github.com>
2024-02-18 20:21:48 +0500

    Update my.ngram.c

--- my.ngram.c
+++ my.ngram.c
@@ -1,34 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int counting_characters(char* word,int *index)
+void counting_characters(const char *sign,int n ,int *index)
 {
-for (int i = 0; word[i] != '\0'; i++) 
+    for (int i = 0; sign[i] != '\0'; i++) 
     {
-        char sign = word[i];
-        if ( sign >= 'a' && sign <= 'z') {
-          index[sign - 'a']++;
-        } else if (sign >= 'A' && sign <= 'Z') {
-            index[letter - 'A']++;
+        int j = i;
+        int index_1 = 0;
+        for (int j = i; j < i + n && sign[j] != '\0'; j++) 
+        {
+            index_1 = index_1 * 256 + sign[j];
+        }
+        index[index_1]++;
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        if (index[i] > 0)
+        {
+            printf("%c - %d ta\n",i, index[i]);
+            sleep(1);
         }
     }
-    for (int i = 0; i < 26; i++) 
-    {
-        if (index[i] > 0) {
-            printf("%c - harfi: %d ta\n", 'a' + i, index[i]);
-               sleep(1);
-        }
-    }
-    return 0;
 }
+
 int main()
 {
-   char* word_arr = calloc(100,sizeof(char));
-   int* index_arr = calloc(26,sizeof(int));
-   printf("SO'Z KIRITING: ");
-   scanf("%s",word_arr);
-   
-   counting_characters(word_arr,index_arr);
-   return 0;
+    char* sign_arr = calloc(256, sizeof(char));
+    int* index_arr = calloc(256, sizeof(int));
+    
+    printf("Enter sign , number or word : ");
+    scanf("%s",sign_arr);
+    
+    counting_characters(sign_arr,1,index_arr);
+    free(sign_arr);
+    free(index_arr);
+    
+    return 0;
 }
